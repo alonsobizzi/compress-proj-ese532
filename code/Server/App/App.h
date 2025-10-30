0b800f851fa80fd05b04e7e74f8eecdb29c4243c
chris <chrisisrael.email@gmail.com>
2025-10-30 10:32:16 -0400

    Minor moves, needs tweaks still, need to do App and get initialization added

---
+++ code/Server/App/App.h
@@ -0,0 +1,10 @@
+#include 
+
+struct controlStruct // Structure containing all parameters that are being fed into the code
+
+//Suspect there will eventually be a command queue lumped into the chunkStructure but unwise to do now
+
+struct chunkStruct // Structure containing transient data for a chunk to be passed to a worker (sequential initially) (Ari made this I think)
+
+
+
