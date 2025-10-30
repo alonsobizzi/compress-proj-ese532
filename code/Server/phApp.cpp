d24bbb44227548f06907e36a23c85887d2602a27
chris <chrisisrael.email@gmail.com>
2025-10-30 02:31:44 -0400

    Moved to right folder, added a pseudocode for part of app that runs every packet (eventually the thing should be a thread?)

---
+++ code/Server/phApp.cpp
@@ -0,0 +1,22 @@
+#include //placeholder here
+
+int appIter(unsigned char* inputBuf, unsigned char* outputBuf, unsigned char* outputChunk
+          shaSoc){
+          
+          unsigned char* ShaTarg = somearraythingIneedtothinkabout; //Just pass with copy for now
+          
+  *newChunkIP, newChunkLen = CDC(someinputoftheabove); // We need to know if we have a new chunk or not. If so, let's go / we need init point and length of new chunk
+  
+  if(NEWCHUNK){
+    compute_sha3_hash(shaSoc, newChunkIP, newChunkLen,  ShaTarg); // Hash whole chunk. Really it is better if we did this incrementally
+    checkMatch(matchID,
+    if (matchID==0) { //If we match
+    sendDupChunk(matchID, outputBuf)
+    }
+    else{
+    //Theoretically run these concurrent
+    opBufPrep(outputBuf, newChunkLen); (
+    LZW(newChunkIP, newChunkLen, outputBuf); //Start writing to out
+    }
+  }
+}
