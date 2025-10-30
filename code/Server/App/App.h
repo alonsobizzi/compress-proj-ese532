2e1a906e4d652f42b19e44edc832145bc3cfa929
chris <chrisisrael.email@gmail.com>
2025-10-30 17:34:59 -0400

    Minor bugfixes, trying to integrate. Some considerations / pseudocode present with cdc integration. Need to build a struct to hold initalization stuff / figure out where to keep cdc struct

--- code/Server/App/App.h
+++ code/Server/App/App.h
@@ -1,10 +1,10 @@
 #include <stdint.h>
 
-struct controlStruct // Structure containing all parameters that are being fed into the code
+//struct controlStruct // Structure containing all parameters that are being fed into the code
 
 //Suspect there will eventually be a command queue lumped into the chunkStructure but unwise to do now
 
-struct chunkStruct // Structure containing transient data for a chunk to be passed to a worker (sequential initially) (Ari made this I think)
+
 
 
 
